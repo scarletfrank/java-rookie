697b8f9818c0aae588e689115c1181f9da1ad823
furan_scarlet <hferfrank@gmail.com>
2022-01-16 10:09:02 +0800

    boilerplate for java

---
+++ java.boilerplate/src/test/java/pro/scarlet/AppTest.java
@@ -0,0 +1,20 @@
+package pro.scarlet;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
